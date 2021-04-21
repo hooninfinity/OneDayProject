0578f08168bfb82d7117fae5abad8eee4093a527
hooninfinity <hooninfinity@naver.com>
2021-04-21 16:38:40 +0900

    blackjack

---
+++ Java_000_BlackJack/src/com/callor/app/service/PlayerService.java
@@ -0,0 +1,13 @@
+package com.callor.app.service;
+
+import java.util.List;
+
+import com.callor.app.model.DeckVO;
+
+public class PlayerService {
+	
+	List<DeckVO> myDeckList;
+	
+	List<DeckVO> pubDeckList;
+
+}
