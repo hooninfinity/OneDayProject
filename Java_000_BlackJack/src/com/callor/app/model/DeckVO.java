c87eadcf9e77da7ed940ab3f453c73034fa2b217
hooninfinity <hooninfinity@naver.com>
2021-04-25 20:37:46 +0900

    BlackJack 2021-04-25

--- Java_000_BlackJack/src/com/callor/app/model/DeckVO.java
+++ Java_000_BlackJack/src/com/callor/app/model/DeckVO.java
@@ -1,7 +1,7 @@
 package com.callor.app.model;
-
+// 입력된 데이터나 조건을 담을 클래스
 public class DeckVO {
-	
+	// 덱 생성에 필요한 무늬, 숫자, 점수 선언
 	private String suit;
 	private String denomination;
 	private Integer value = 0;
@@ -11,7 +11,7 @@ public class DeckVO {
 		// TODO Auto-generated constructor stub
 	}
 	
-	
+	// this명령어로 현재 클래스의 멤버변수 지정해서 생성
 	public DeckVO(String suit, String denomination, Integer value) {
 		super();
 		this.suit = suit;
@@ -20,7 +20,7 @@ public class DeckVO {
 	}
 
 
-
+	// getter, setter 생성
 	public String getSuit() {
 		return suit;
 	}
@@ -39,6 +39,7 @@ public class DeckVO {
 	public void setValue(Integer value) {
 		this.value = value;
 	}
+	// 생성된 카드 출력에 필요한 toString 작성
 	@Override
 	public String toString() {
 		return " [무늬=" + suit + ", 숫자=" + denomination + ", 점수=" + value + "]";
--- Java_000_BlackJack/src/com/callor/app/service/DeckServiceV1.java
+++ Java_000_BlackJack/src/com/callor/app/service/DeckServiceV1.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Random;
 
 import com.callor.app.model.DeckVO;
-
+// 사용할 카드덱을 생성하는 클래스
 public class DeckServiceV1 {
-	
+	// 덱리스트, 무늬, 숫자
 	protected List<DeckVO> deckList;
 	String strSuit = "다이아몬드(◆):하트(♡):스페이드(♠):클로버(♣)";
 	String strDenomi = "A234567890KQJ";
@@ -22,48 +22,51 @@ public class DeckServiceV1 {
 //		deckList.remove(deckIndex);
 //		return retDeckVO;
 //	}
-	
+	// DeckVO 타입의 List getDeck
 	public List<DeckVO> getDeck(){
 		for(DeckVO vo : deckList) {
+			// deckList의 내용을 랜덤형태로 보여줌
 			Collections.shuffle(this.deckList);
 		}
 		return this.deckList;
 	}
 	
 	public DeckServiceV1() {
-		// TODO Auto-generated constructor stub
+		// DeckVO 타입의 ArrayList(객체들이 추가되어 자동으로 용량이 추가됨) 생성
 		deckList = new ArrayList<DeckVO>();
 	}
 	
+	// makeDeck() method에서 카드 생성
 	public void makeDeck() {
-		
+		// A234567890KQJ 문자열을 문자별로 쪼갠후 denoms 배열에 담음
+		// 다이아몬드(◆):하트(♡):스페이드(♠):클로버(♣) 문자열을 : 을 기준으로 suit에 담음
 		String[] denoms = strDenomi.split("");
 		String[] suits = strSuit.split(":");
 		
-		// 4번 반복
+		// 무늬 suits를 suit 변수에 한번 담을때마다 문자열 A234567890KQJ 반복
 		for(String suit : suits) {
 			for(String denom : denoms) {
-				
+				// 예외처리문을 사용해서 2~9 까지의 값은 그대로 점수 생성
+				// 숫자가 0이면 10점
 				Integer intValue = 0;
 				try {
 					intValue = Integer.valueOf(denom);
 					if (intValue == 0) intValue = 10;
 				} catch (NumberFormatException e) {
 					// denom 문자열이 A,K,Q,J인 경우 Exception 발생, 이걸 이용
+					// A면 값이 1, 나머지 경우의 수 K,Q,J는 10으로 지정
 					if (denom.equals("A")) intValue = 1;
 					else intValue = 10;
 				}
+				// 생성이 끝나면 그 값을 각자 deckVO에 셋팅해주고 리스트에 추가
 				DeckVO deckVO = new DeckVO();
 				deckVO.setSuit(suit);
 				deckVO.setDenomination(denom);
 				deckVO.setValue(intValue);
 				deckList.add(deckVO);
-				
-//				System.out.println(deckVO.toString());
 			}
 		}
-		
-	}
+	} // end makeDeck
 	
 	
 
